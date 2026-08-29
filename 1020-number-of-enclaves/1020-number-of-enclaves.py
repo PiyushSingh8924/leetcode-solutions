0fb91ba0eaaf0e9744f4a4bd4720b19bb8981b10
Piyush kumar Singh <piyushkmsingh321@gmail.com>
2026-08-30 03:15:12 +0530

    Time: 134 ms (41.43%), Space: 13.3 MB (83.33%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,17 +1,4 @@
 class Solution(object):
-    def dfs(self, r, c, visited, rows, cols, grid): 
-            if r < 0 or r >= rows or c < 0 or c >= cols: 
-                return 
-            if visited[r][c] == 1: 
-                return 
-            if grid[r][c] == 0: 
-                return 
-            visited[r][c] = 1 
-            self.dfs(r, c - 1, visited, rows, cols, grid) 
-            self.dfs(r - 1, c, visited, rows, cols, grid) 
-            self.dfs(r, c + 1, visited, rows, cols, grid) 
-            self.dfs(r + 1, c, visited, rows, cols, grid)
-
     def numEnclaves(self, grid):
         """
         :type grid: List[List[int]]
@@ -20,14 +7,29 @@ class Solution(object):
         rows = len(grid)
         cols = len(grid[0])
         visited =[[0 for _ in range(cols)] for _ in range(rows)]
+        queue = deque()
         for r in range(rows):
             for c in range(cols):
-                if (r == 0 or r == rows - 1 or c == 0 or c == cols - 1):
-                    if grid[r][c] == 1: 
-                        self.dfs(r, c, visited, rows, cols, grid)
+                if r == 0 or r == rows - 1 or c == 0 or c == cols - 1:
+                    if grid[r][c] == 1:
+                        queue.append((r,c))
+                        visited[r][c] = 1
+        while queue:
+            i,j = queue.popleft()
+            for x,y in [(1,0),(-1,0),(0,1),(0,-1)]:
+                dx = i + x
+                dy = j + y
+                if dx < 0 or dx >= rows or dy < 0 or dy >= cols:
+                    continue
+                if grid[dx][dy] == 0:
+                    continue
+                if grid[dx][dy] == 1 and visited[dx][dy] == 1:
+                    continue
+                queue.append((dx,dy))
+                visited[dx][dy] = 1
         count = 0
         for r in range(rows):
             for c in range(cols):
-                if visited[r][c] == 0 and grid[r][c] == 1:
+                if grid[r][c] == 1 and visited[r][c] == 0:
                     count += 1
         return count
