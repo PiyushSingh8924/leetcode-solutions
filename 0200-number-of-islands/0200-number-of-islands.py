78b62df664098b04fd02132898707eb426ec5453
Piyush kumar Singh <piyushkmsingh321@gmail.com>
2026-08-31 00:50:24 +0530

    Time: 250 ms (24.04%), Space: 29.6 MB (22.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,34 @@
 class Solution(object):
+    def dfs(self,i,j,visited,grid):
+        rows = len(grid)
+        cols = len(grid[0])
+        if i < 0 or j < 0 or i >= rows or j >= cols:
+            return
+        if visited[i][j] == 1:
+            return
+        if grid[i][j] == "0":
+            return
+        visited[i][j] = 1
+        self.dfs(i+1,j,visited,grid)
+        self.dfs(i-1,j,visited,grid) 
+        self.dfs(i,j+1,visited,grid) 
+        self.dfs(i,j-1,visited,grid) 
+
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid:
-            return 0
+        count = 0
         rows = len(grid)
         cols = len(grid[0])
-        islands = 0
-
+        visited = [[0 for _ in range(cols)]for _ in range(rows)]
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1':
-                    islands += 1
-                    queue = deque([(r, c)])
-                    grid[r][c] = '0'
-                    while queue:
-                        x, y = queue.popleft()
-                        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                            new_i, new_j = x + dx, y + dy
-                            if (0 <= new_i < rows and 0 <= new_j < cols and grid[new_i][new_j] == '1'):
-                                grid[new_i][new_j] = '0'
-                                queue.append((new_i, new_j))
-        return islands
+                if grid[r][c] == "1" and visited[r][c] ==0:
+                    count += 1
+                    self.dfs(r,c,visited,grid)
+        return count
+
+                
+    
