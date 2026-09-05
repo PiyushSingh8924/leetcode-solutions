bac3034f5cb61460133e514cb7abd74ecb210035
Piyush kumar Singh <piyushkmsingh321@gmail.com>
2026-09-05 17:54:02 +0530

    Time: 8 ms (27.51%), Space: 13.1 MB (70.71%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        adj_list = [[]for _ in range(numCourses)]
+        indegrees = [0 for _ in range(numCourses)]
+        for u,v in prerequisites:
+            adj_list[v].append(u)
+            indegrees[u] += 1
+        queue = deque()
+        for i in range(0,numCourses):
+            if indegrees[i] == 0:
+                queue.append(i)
+        result = []
+        while queue:
+            curr_node = queue.popleft()
+            result.append(curr_node)
+            for adj_node in adj_list[curr_node]:
+                indegrees[adj_node] -= 1
+                if indegrees[adj_node] == 0:
+                    queue.append(adj_node)
+        if len(result) == numCourses:
+            return result 
+        return []
+
+
